63f4204ab94897b6a374615c7880e7ca7ea41295
Riddhi Basu <riddhibasu100stars@gmail.com>
2026-03-09 21:08:58 +0530

    Binary Search : 1 problem solved

---
+++ BinarySearch/Sqrt(x).py
@@ -0,0 +1,11 @@
+class Solution:
+    def mySqrt(self, x: int) -> int:
+        left = 1
+        right = x
+        while left <= right :
+            mid = (left + right) // 2
+            if mid > x // mid :
+                right = mid - 1
+            else :
+                left = mid + 1
+        return right
