4859179026832dd473ae6777b2c555880c98efa9
Riddhi Basu <riddhibasu100stars@gmail.com>
2026-03-16 20:44:21 +0530

    First Bad Version : code modified.

---
+++ BinarySearch/first_bad_version.py
@@ -0,0 +1,23 @@
+# The isBadVersion API is provided by LeetCode.
+# def isBadVersion(version: int) -> bool:
+# mock function for local testing
+bad = 4
+
+def isBadVersion(version):
+    return version >= bad
+
+
+class Solution:
+    def firstBadVersion(self, n: int) -> int:
+        left = 1
+        right = n
+
+        while left < right:
+            mid = (left + right) // 2
+
+            if isBadVersion(mid):
+                right = mid
+            else:
+                left = mid + 1
+
+        return left
