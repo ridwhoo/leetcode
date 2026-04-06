43ab09d55ede22c6ab19c121f7be90426fa32dae
Riddhi Basu <riddhibasu100stars@gmail.com>
2026-04-06 20:32:23 +0530

    LC 905 Sort Array by Parity solve using two pointer approach.

---
+++ Arrays/sort_by_parity.py
@@ -0,0 +1,12 @@
+from typing import List
+class Solution:
+    def sortArrayByParity(self, nums: List[int]) -> List[int]:
+        left = 0
+        right = len(nums) - 1
+        while left < right :
+            if nums[left] % 2 == 0 :
+                left += 1
+            else :
+                nums[left] , nums[right] = nums[right] , nums[left]
+                right -= 1    
+        return nums        
