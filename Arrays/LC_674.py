6d47a6e42fdd628ce681ac8df62081be3f58d7b2
Riddhi Basu <riddhibasu100stars@gmail.com>
2026-04-09 21:08:55 +0530

    LC 647 added by using streak/run length tracking.

---
+++ Arrays/LC_674.py
@@ -0,0 +1,13 @@
+from typing import List
+class Solution:
+    def findLengthOfLCIS(self, nums: List[int]) -> int:
+        curr_len = 1
+        max_len = 1
+        for i in range (1, len(nums)) :
+            if nums[i] > nums[i-1] :
+                curr_len += 1
+            else :
+                curr_len = 1
+            max_len = max(max_len, curr_len)
+        return max_len
+                        
